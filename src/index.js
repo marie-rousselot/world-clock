00181c8a4ae150d438d47c75f30cfb9b871142fc
marie-rousselot <159030042+marie-rousselot@users.noreply.github.com>
2024-04-14 16:30:30 +0800

    Added link to all cities

--- src/index.js
+++ src/index.js
@@ -41,7 +41,9 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )}<small>${cityTime.format(":SSS A")}</small></div>
-        </div>`;
+        </div>
+        <a href="/">All cities</a>
+        `;
 }
 updateTime();
 setInterval(updateTime, 1);
